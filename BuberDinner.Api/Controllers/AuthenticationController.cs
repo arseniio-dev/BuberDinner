93d307cfa87fb01768956f78e3bc3c4ff29c055c
Arsenii Oboznyi <dimont075@gmail.com>
2023-12-02 15:58:52 +0100

    [API/Authentication] Add Controller and Requests Bases

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[Route("Auth")]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        return Ok();
+    }
+    
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login(LoginRequest request)
+    {
+        return Ok();
+    }
+}
