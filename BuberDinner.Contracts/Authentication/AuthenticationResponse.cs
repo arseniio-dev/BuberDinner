9fda3541d5b978aa3eb97d9fe89e4709fa76d25e
Arsenii Oboznyi <dimont075@gmail.com>
2023-12-02 15:57:07 +0100

    [Contracts/Authentication] Add Authentication Contracts

---
+++ BuberDinner.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BuberDinner.Contracts.Authentication;
+
+public record AuthenticationResponse(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Token);
