bf9030929f55114b46e130186bd4fd64f20f61ec
Arsenii Oboznyi <dimont075@gmail.com>
2023-12-13 23:55:03 +0100

    [User] Create User Entity

---
+++ BuberDinner.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace BuberDinner.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public string FirstName { get; init; } = null!;
+
+    public string LastName { get; init; } = null!;
+
+    public string Email { get; init; } = null!;
+
+    public string Password { get; init; } = null!;
+}
