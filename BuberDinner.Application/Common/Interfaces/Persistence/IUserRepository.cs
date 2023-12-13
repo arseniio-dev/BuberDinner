f8ac442ff29a986cb5393b51bf1ea63867bf0f9a
Arsenii Oboznyi <dimont075@gmail.com>
2023-12-13 23:56:03 +0100

    [User] Create IUserRepository.cs and implement it to persist data in List at runtime (DB TDB)

---
+++ BuberDinner.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using BuberDinner.Domain.Entities;
+
+namespace BuberDinner.Application.Common.Interfaces.Persistence;
+
+public interface IUserRepository
+{
+    User? GetUserByEmail(string email);
+
+    User? GetUserById(Guid id);
+    
+    void Add(User user);
+}
---
+++ BuberDinner.Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Entities;
+
+namespace BuberDinner.Infrastructure.Persistence;
+
+public class UserRepository : IUserRepository
+{
+    private static readonly List<User> Users = new List<User>();
+    
+    public User? GetUserByEmail(string email)
+    {
+        return Users.SingleOrDefault(user => user.Email == email);
+    }
+
+    public User? GetUserById(Guid id)
+    {
+        return Users.SingleOrDefault(user => user.Id == id);
+    }
+
+    public void Add(User user)
+    {
+        Users.Add(user);
+    }
+}
